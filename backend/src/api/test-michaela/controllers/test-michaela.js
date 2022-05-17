4a1ea1efe1d1d279cf709aafd521124a9373b466
Soheyl Rahgozar <soheyl.rahgozar@ericsson.com>
2022-05-17 17:17:56 -0400

    action-boost-dashboard-golabal-internal

--- backend/src/api/test-michaela/controllers/test-michaela.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  test-michaela controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::test-michaela.test-michaela');
--- backend/src/api/test-michaela/services/test-michaela.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * test-michaela service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::test-michaela.test-michaela');
